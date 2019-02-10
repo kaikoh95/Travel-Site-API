7e80b11a7a97e562850b6471067ff62f6b1e2a3e
Alex Tompkins <alextompkins3@gmail.com>
2019-02-10 23:36:07 +1300

    Add debug for docker CI

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const db = require('./config/db');
 const express = require('./config/express');
 
 const app = express();
-const port = process.env.PORT || 4941;
+const port = 4941;
 
 // Test connection to MySQL on start-up
 async function testDbConnection() {
+    console.log(`
+    SENG365_PORT: ${process.env.SENG365_PORT}
+    SENG365_HOST: ${process.env.SENG365_HOST}
+    SENG365_USER: ${process.env.SENG365_USER}
+    SENG365_PASSWORD: ${process.env.SENG365_PASSWORD}
+    SENG365_DATABASE: ${process.env.SENG365_DATABASE}
+    `);
     try {
         await db.createPool();
         await db.getPool().getConnection();
     } catch (err) {
-        console.error('Unable to connect to MySQL.');
+        console.error(`Unable to connect to MySQL: ${err.message}`);
         process.exit(1);
     }
 }
