295d116a3c3be4580c69a6984bc05252ea57351d
kurokikai95 <kaikoh95@gmail.com>
2019-03-25 06:15:54 +1300

    Develop: added function to generate random int

---
+++ app/helpers/generate_random_integer.js
@@ -0,0 +1,6 @@
+
+exports.getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
